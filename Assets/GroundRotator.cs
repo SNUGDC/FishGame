da7e1d6d9bc9be6cac00ce5803be4ad93f45d841
MyeongzinSon@Icheon <sonmyungzin1@naver.com>
2018-02-07 01:26:39 +0900

    돌아가는 삼각형 구현(테스트 아직 못해봄)

---
+++ Assets/GroundRotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundRotator : MonoBehaviour {
+
+	public float timePerRoll = 3;
+	public bool isClockwise;
+	void Update () {
+		if (isClockwise)
+		{
+			transform.Rotate(0, 0, -360 * Time.deltaTime / timePerRoll);
+		}
+		else
+		{
+			transform.Rotate(0, 0, 360 * Time.deltaTime / timePerRoll);
+		}
+	}
+}
