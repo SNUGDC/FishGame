6de215cb5c540d439b379e2dfb42520e608a77ee
DongRyeong Kim <birdwannafly@gmail.com>
2018-02-15 21:25:48 +0900

    Ground object 위아래에서만 점프될수 있도록 싹다 바꿔줌. 이제 맵짤때 ground prefab으로 짜지 말고 ground_2 를 가지고 맵만들면 됨 + 닿은 면을 기준으로 해서 각도 설정하도록 코드 설정해줌
    **playercontroller에 주석처리한건 테스트용으로 짜놓은거니까 지우지 마세요

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,7 +17,7 @@ public class PlayerController : MonoBehaviour {
 	   	PV.accY_now = Input.acceleration.y;
 	}//initialize
 	void Update () {
-		/*
+		
 		PV.touchcount = Input.touchCount;
 		GetDeltaAccY();
 		Debug.Log(Input.acceleration.x + ", "+Input.acceleration.y + ", " + Input.acceleration.z);
@@ -28,10 +28,10 @@ public class PlayerController : MonoBehaviour {
 			rb.velocity =new Vector2(
 				tilt.x*PV.grad.x-tilt.y*PV.grad.y,
 				tilt.y*PV.grad.x+tilt.x*PV.grad.y
-			)*5f;
+			)*20f;
 		}
-		*/
 
+		/*
 		if (PV.inwater == true || PV.land == true) {
 			if (Input.GetKeyDown ("d")) {
 				Debug.Log ("d pressed");
@@ -49,37 +49,47 @@ public class PlayerController : MonoBehaviour {
 				) * 10f;
 			}
 		}
+		*/
 	}
 
 
-	void OnTriggerStay2D(Collider2D other){
+	void OnTriggerEnter2D(Collider2D other){
 		if (other.tag == "Water") {
 			inwater_do (other);
 		} 
 		if (other.tag == "Obstacle"){
-			obsatcle_do();
+			obstacle_do();
 		}
-		if (other.transform.parent.tag == "Ground") {
-			land_do ();
-			if (other.tag == "Ground_Top") {
-				setgrad ((other.transform.eulerAngles.z / 180) * Mathf.PI);
-			} 
-			if (other.tag == "Ground_Bottom") {
-				setgrad ((other.transform.eulerAngles.z + 180) / 180 * Mathf.PI);
-			} 
-			if (other.tag == "Ground_60") {
-				setgrad ((other.transform.eulerAngles.z + 60) / 180 * Mathf.PI);
-			} 
-			if (other.tag == "Ground_-60") {
-				setgrad ((other.transform.eulerAngles.z - 60) / 180 * Mathf.PI);
-			} 
+		if (other.transform.parent != null) {
+			if (other.transform.parent.tag == "Ground") {
+
+				land_not_do ();
+			
+				if (other.tag == "Ground_Top") {
+					setgrad ((other.transform.eulerAngles.z / 180) * Mathf.PI);
+					land_do ();
+				} 
+				if (other.tag == "Ground_Bottom") {
+					setgrad ((other.transform.eulerAngles.z + 180) / 180 * Mathf.PI);
+					land_do ();
+				} 
+				if (other.tag == "Ground_60") {
+					setgrad ((other.transform.eulerAngles.z + 60) / 180 * Mathf.PI);
+					land_do ();
+				} 
+				if (other.tag == "Ground_-60") {
+					setgrad ((other.transform.eulerAngles.z - 60) / 180 * Mathf.PI);
+					land_do ();
+				} 
+			}
 		}
 	}
 	void OnTriggerExit2D(Collider2D other){
 		if (other.tag == "Water") {
 			inwater_not_do ();
 		}
-		if (other.transform.parent.tag == "Ground") {
+	    if (other.transform.parent!=null) {
+			if(other.transform.parent.tag=="Ground")
 			land_not_do ();
 		}
 
@@ -122,9 +132,11 @@ public class PlayerController : MonoBehaviour {
 		Debug.Log ("land_not_do called");
 		PV.land = false;
 	}
-	void obsatcle_do()
+	void obstacle_do()
 	{
+		Debug.Log ("obstacle_do called");
 		rb.velocity = new Vector2(-1 * rb.velocity.x, Input.acceleration.y * 20);
+		Debug.Log ("obstacle_do called2");
 	}
 	void setgrad(float degree){
 		PV.grad= new Vector2 (Mathf.Cos (degree), Mathf.Sin (degree));
