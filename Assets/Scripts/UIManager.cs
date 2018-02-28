9608dd65cce0e026646b04505f4fa6e1df00d21b
Myeongzin Son <sonmyungzin1@naver.com>
2018-02-28 23:35:46 +0900

    메인 화면, 팝업 추가

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+	public GameObject popUp;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void MoveScene(string sceneName){
+		SceneManager.LoadScene(sceneName);
+	}
+	public void QuitGame(){
+		Application.Quit();
+	}
+	public void SetPopUpActive(bool b){
+		popUp.SetActive(b);
+	}
+}
